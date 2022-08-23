9fb25cb3ddef9f4b2c17da4e0569236d7c1ef9f4
Neerav Singla <neeravsingla1526@gmail.com>
2022-08-23 12:12:25 +0530

    commit

---
+++ src/Components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Header(){
+    return (
+        <div className="center header">
+        <h1> Calulator - App </h1>
+        </div>
+    );
+}
+
+export default Header; 
